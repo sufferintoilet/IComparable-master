0817c4e1c1a1388dc5b8a373e10cda726ff4708a
sufferintoilet <nguyenhuynhquocan1@gmail.com>
2024-01-16 15:49:11 +0900

    1

---
+++ MyCircle.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+
+namespace Shape
+{
+  public class MyCircle : IComparer<Circle>
+  {
+
+    public int Compare(Circle c1, Circle c2)
+    {
+      if (c1.getRadius() > c2.getRadius()) return 1;
+      else if (c1.getRadius() < c2.getRadius()) return -1;
+      else return 1;
+    }
+  }
+}
